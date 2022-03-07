8e3567a6542987cd34638b9911856012837b4c68
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-03-06 21:24:35 -0300

    Implementando a camada de dados

---
+++ Mercadinho.Data/Repositories/BaseRepositorio.cs
@@ -0,0 +1,39 @@
+﻿using Mercadinho.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace Mercadinho.Data.Repositories
+{
+    public class BaseRepositorio<T> : IBaseRespositorio<T> where T : class, IEntidade
+    {
+        public void Atualizar(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Criar(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T SelecionarPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
