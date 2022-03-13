bd2cb18106d05601b4541b868236f4c405bc9e1b
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-03-12 21:02:50 -0300

    Adicionando serviços de produto na aplicação

--- Mercadinho.Infra.Ioc/Bootstrap.cs
+++ Mercadinho.Infra.Ioc/Bootstrap.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mercadinho.Application.App;
+using Mercadinho.Application.Interface;
+using Mercadinho.Data;
+using Mercadinho.Data.Repositories;
+using Mercadinho.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Mercadinho.Infra.Ioc
 {
     public class Bootstrap
     {
+        public static void RegistroDeServicos(IServiceCollection services, IConfiguration configuration)
+        {
+            // Repositorios
+            services.AddScoped(typeof(IBaseRespositorio<>), typeof(BaseRepositorio<>));
+            services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
 
+            // Aplicação 
+            services.AddScoped<IProdutoAplicacao, ProdutoAplicacao>();
+
+            services.AddDbContext<Contexto>(options => 
+                options.UseSqlServer(configuration.GetConnectionString(""))    
+            );
+        }
     }
 }
